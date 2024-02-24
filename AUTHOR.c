8e049c104e0004ad2ce1f6901a7efa75ad7d789d
epsonagunda <epsonagunda182@gmail.com>
2024-02-23 23:39:36 -0800

    author.c

---
+++ AUTHOR.c
@@ -0,0 +1,24 @@
+#simple shell
+shell is a computer program that presents a cli which allows you to cc your computer using commands entered with a keyboard instead of controlling graphical user interface (GUI) WITH A MOUSE/KEYBOARD/TOUCHSCREEN
+WHY SHELL
+shell makes your work more reproducible since it keeps a history of your work.
+how to creat shell (kernel)
+	myepson_dir
+	myepson_script.sh
+#!/bin/bash
+	the anatomy of the shell
+	the innermost core is the kernel
+	the outermost shell is the shell
+	the kernel acts as awindow for the software programs to recognize and run on the hardware componets
+	shell receives the commands directly from the user and send it to the kernel for processing and in turn, returns back the response to the user
+
+
+	my type of shell 
+	bourne shell(sh) - the first kind of shell programmed by stwephen r bourne in AT&T bell lab.
+	also known as the primary unix shell eg ksh,bash,sh
+	c shell (csh) - the UNIX  enhancement written by bill joy
+
+	shell prompt
+	the prompt $ is called the command prompt, which display by the shell.
+	its an interface which you can write and execute your commands and programs.
+
